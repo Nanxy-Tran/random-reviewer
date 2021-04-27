7c38f68a98a610c29d986de22c1aa9105168087f
Q.Thinh <eeit104@gmail.com>
2021-04-27 10:25:25 +0700

    fix typescript

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,29 @@
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 109:
-/***/ (function(__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,8 +34,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const core = __nccwpck_require__(186);
-const github = __nccwpck_require__(438);
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(186));
+const github = __importStar(__nccwpck_require__(438));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const context = github.context;
     if (context.payload.pull_request == null) {
@@ -32,7 +52,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         throw new Error("List of reviewers is not provided !");
     const addAuthor = () => __awaiter(void 0, void 0, void 0, function* () {
         core.info(context.actor);
-        yield octokit.pulls.addAssignees(Object.assign(Object.assign({}, context.repo), { issue_number: context.issue.number, assignees: [context.actor] }));
+        yield octokit.issues.addAssignees(Object.assign(Object.assign({}, context.repo), { issue_number: context.issue.number, assignees: [context.actor] }));
     });
     const addReviewers = (reviewers, numberReviewers, isRandomReview) => __awaiter(void 0, void 0, void 0, function* () {
         var _a;
