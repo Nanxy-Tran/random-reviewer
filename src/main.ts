7e4baf5a9b7519aa282f49621ee4e952bcafcae2
Q.Thinh <eeit104@gmail.com>
2021-04-27 10:12:58 +0700

    reviewer v1

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const main = async () => {
+  const context = github.context;
+  if (context.payload.pull_request == null) {
+    core.setFailed("No pull request found.");
+    return;
+  }
+
+  const githubToken = core.getInput("token");
+  const numberReviewers = core.getInput("numberReviewers");
+  const isRandomReview = core.getInput("randomReview");
+  const reviewers = core.getInput("reviewers");
+  const octokit = github.getOctokit(githubToken);
+  if (!reviewers || reviewers.length < 1)
+    throw new Error("List of reviewers is not provided !");
+
+  const addAuthor = async () => {
+    core.info(context.actor);
+    await octokit.pulls.addAssignees({
+      ...context.repo,
+      issue_number: context.issue.number,
+      assignees: [context.actor],
+    });
+  };
+
+  const addReviewers = async (reviewers, numberReviewers, isRandomReview) => {
+    await octokit.pulls.requestReviewers({
+      ...context.repo,
+      reviewers: isRandomReview
+        ? randomReviewers(reviewers, numberReviewers)
+        : reviewers,
+      pull_number: context.payload.pull_request?.number,
+    });
+  };
+
+  const randomReviewers = (reviewers, numberReviewers) => {
+    const author = context.actor;
+    const availableReviewers = reviewers.filter(
+      (reviewer) => reviewer !== author
+    );
+    let result: any = [];
+    while (result.length < numberReviewers)
+      result.push(
+        availableReviewers[
+          Math.floor(Math.random() * availableReviewers.length)
+        ]
+      );
+    return result;
+  };
+
+  await addAuthor();
+  await addReviewers(reviewers, numberReviewers, isRandomReview);
+};
+
+main().catch((err) => core.setFailed(err));
