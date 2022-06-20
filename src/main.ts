1f53c56b7a8dba65dc20bdc67b3ec081fd056c7f
Quoc Thinh <eeit104@gmail.com>
2022-06-20 12:10:34 +0700

    small change 1

--- src/main.ts
+++ src/main.ts
@@ -57,6 +57,7 @@ const main = async () => {
       pull_number: context.payload.pull_request?.number,
     });
     core.info(`Auto assign pull request to reviewers successfully ! `);
+    core.info("Small change to trigger workflow")
     core.info(
       "This PR is randomly assigned: " +
         isRandomReview +
